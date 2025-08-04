f49a4ffd6e8d4732c2abc6516ddac8e3fad69954
Linda de Sol <formumm@yahoo.de>
2025-08-04 11:46:32 +0200

    update home and dashboard page, refactor comments

--- resources/views/components/no-results-message.blade.php
+++ resources/views/components/no-results-message.blade.php
@@ -1,7 +1,11 @@
-@props(['query'])
+@props(['query' => null])
 
 <div class="mt-4 p-4 border border-yellow-300 dark:border-yellow-600 rounded 
             bg-yellow-50 dark:bg-yellow-900/20 
             text-yellow-800 dark:text-yellow-200">
-    <p>No movies found with the title "{{ $query }}".</p>
+    @if($query)
+        <p>No movies found with the title "{{ $query }}".</p>
+    @else
+        <p>You haven't rated any movies yet. Start rating movies to see them here!</p>
+    @endif
 </div>
